9bf9767cc5829ed340820ee4b9ce0854c96bdc64
jejolare <jejolare@gmail.com>
2025-08-04 01:15:42 +0700

    update intervals

--- api/utils/mexc.ts
+++ api/utils/mexc.ts
@@ -28,7 +28,7 @@ export function getStats(klines: Kline[]): SimpleStats {
 
     const changePercent = ((price - openPrice) / openPrice) * 100;
 
-    const volume = klines.reduce((sum, k) => sum + parseFloat(k[7]), 0);
+    const volume = klines.reduce((sum, k) => sum + parseFloat(k[5]), 0);
 
     return {
         price,
@@ -40,29 +40,33 @@ export function getStats(klines: Kline[]): SimpleStats {
 export async function fetchMexcData(term: 'day' | 'month' | 'year', symbol: string): Promise<SimpleStats> {
 
 
-    const startTime = (() => {
+    const {startTime, interval} = (() => {
         const now = Date.now();
         let startTime = 0;
+        let interval = '1d';
 
         switch (term) {
             case "day":
                 startTime = now - 24 * 60 * 60 * 1000;
+                interval = '5m';
                 break;
             case "month":
                 startTime = now - 30 * 24 * 60 * 60 * 1000;
+                interval = '1d';
                 break;
             case "year":
                 startTime = now - 365 * 24 * 60 * 60 * 1000;
+                interval = '1d';
                 break;
             default:
                 throw new Error("Invalid term for fetching Zano data");
         }
 
-        return startTime;
+        return {startTime, interval};
     })();
 
-    console.log(`https://api.mexc.com/api/v3/klines?symbol=${symbol}&interval=1d&startTime=${startTime}&endTime=${+new Date()}`);
-    
+    console.log(`https://api.mexc.com/api/v3/klines?symbol=${symbol}&interval=${interval}&startTime=${startTime}&endTime=${+new Date()}`);
+
 
     const response = await fetch(
         `https://api.mexc.com/api/v3/klines?symbol=${symbol}&interval=1d&startTime=${startTime}&endTime=${+new Date()}`
